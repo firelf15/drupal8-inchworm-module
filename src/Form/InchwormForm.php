68a4ed6efb5c15ddc842361c9fbd59704f38b70d
FirElf15 <github@firelf.com>
2016-06-03 18:17:38 -0700

    there is a form of sorts

---
+++ src/Form/InchwormForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\inchworm\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class InchwormForm extends FormBase {
+  public function getFormId() {
+    // Unique ID of the form.
+    return 'inchworm_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Create a $form API array.
+    $form['first_number'] = array(
+      '#type' => 'number',
+      '#title' => $this->t('Pick a number, any number.')
+    );
+    $form['second_number'] = array(
+      '#type' => 'number',
+      '#title' => $this->t('Pick a number, another number.')
+    );
+    return $form;
+  }
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Validate submitted form data.
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Handle submitted form data.
+  }
+}
