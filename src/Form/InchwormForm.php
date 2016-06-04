cd92d49338eefc45d4e96e93ca40f1cf1bb22ea9
FirElf15 <github@firelf.com>
2016-06-03 21:27:39 -0700

    numeric validation.

--- src/Form/InchwormForm.php
+++ src/Form/InchwormForm.php
@@ -30,6 +30,12 @@ class InchwormForm extends FormBase {
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
     // Validate submitted form data.
+    if (!is_numeric($form_state->getValue('first_number'))) {
+      $form_state->setErrorByName('first_number', $this->t("Something has gone horribly wrong with your number picking or my code writing. I hope it's you. Please try again with your first number."));
+    }
+    if (!is_numeric($form_state->getValue('second_number'))) {
+      $form_state->setErrorByName('second_number', $this->t("Something has gone awry. It's not me. It's you. I hope. Please try again with your second number."));
+    }
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
